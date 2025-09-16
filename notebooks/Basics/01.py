859637f03a8a866da822bfe573248935aff82643
Harrison Sowden <harrison.sowden609@gmail.com>
2025-09-16 19:01:01 -0400

    Add Lesson 1 script (01.py) in Basics folder

---
+++ notebooks/Basics/01.py
@@ -0,0 +1,14 @@
+# Lesson 1: Variables & Data Types
+
+# Strings
+name = "Harrison"
+city = "Trenton"
+
+# Numbers
+age = 30
+gpa = 3.4
+
+# Boolean
+is_student = True
+
+print(name, age, gpa, is_student)
